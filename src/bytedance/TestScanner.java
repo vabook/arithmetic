4caa805bd028132795f41d0532170c6574167b5c
vabook <vabook@163.com>
2019-04-26 10:46:18 +0800

    测试Scanner类用法

--- src/bytedance/TestScanner.java
+++ src/bytedance/TestScanner.java
@@ -25,7 +25,7 @@ public class TestScanner {
 		System.out.println(aa);*/
 		
 		//单独用next()方法获取字符串，输入tab、space、enter时，会从这里为断点，只会获取之前的数据
-//		String  string = scanner.next();
+		//String  string = scanner.next();
 		//单独用in.nextLine()方法获取一行输入的数据,无论输入tab、space都会完整的获取到，当输入enter时，表示输入完毕。
 		//如果使用完in.next()后没有关闭，就使用in.nextLine()，则in.nextLine()会获取前一方法截断后的数据
 		
