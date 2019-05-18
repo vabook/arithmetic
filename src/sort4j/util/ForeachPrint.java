1bf0d8f73429c582645bff1c900e111b19701b3f
vabook <vabook@163.com>
2019-05-18 17:08:28 +0800

    工具类

---
+++ src/sort4j/util/ForeachPrint.java
@@ -0,0 +1,13 @@
+package sort4j.util;
+/**
+* @author vabook@163.com
+* @date 2019年5月18日 上午10:37:10
+*
+*/
+public class ForeachPrint {
+	public static void foreachPrint(int[] array) {
+		for(int i = 0 ; i < array.length; i ++) {
+			System.out.print(array[i] + " ");
+		}
+	}
+}
